b50fb6040f76fe6dccbacf3c780f1c27e775b128
sandro <iashvilisandro7@gmail.com>
2023-01-22 14:22:24 +0400

    Chagnes

--- viginer.py
+++ viginer.py
@@ -27,8 +27,6 @@ f1.grid(row=1, column=0, sticky="nsew", pady=0)
 
 
 
-
-
 Bottom = customtkinter.CTkFrame(master=root, fg_color="#E5E0FF")
 Bottom.grid(row=2, column=0, sticky="nsew")
 Bottom.configure(height=50)
@@ -46,6 +44,7 @@ localtime = time.asctime(time.localtime(time.time()))
 lblInfo = customtkinter.CTkLabel(Tops, font = ('helvetica', 30),
 		text = "ვიჟინერის მეთოდი", text_color="#FFF2F2",anchor="center")
 lblInfo.pack(side=TOP, anchor="center")
+
 
 lblInfo = customtkinter.CTkLabel(Tops, font=('arial', 20),
 text = localtime, text_color="#E5E0FF",
@@ -68,29 +67,29 @@ def Reset():
 	rand.set("")
 	Msg.set("")
 	key.set("")
-	mode.set("")
+	mode.set(0)
 	Result.set("")
 
 
 # reference
 
-lblReference = customtkinter.CTkLabel(master=f1,
-                               text="სახელი",
-                               text_color="#FFFFE8",
-                               width=200,
-                               height=60, font = ('arial', 25))
+# lblReference = customtkinter.CTkLabel(master=f1,
+#                                text="სახელი",
+#                                text_color="#FFFFE8",
+#                                width=200,
+#                                height=60, font = ('arial', 25))
 				
-lblReference.grid(row = 0, column = 0,pady=8)
+# lblReference.grid(row = 0, column = 0,pady=8)
 
-txtReference = customtkinter.CTkEntry(master=f1,
-                               textvariable=rand,
-                               width=200,
-                               height=50,
-                               fg_color="#FFF2F2",
-                               border_width=3,
-                               corner_radius=10)
+# txtReference = customtkinter.CTkEntry(master=f1,
+#                                textvariable=rand,
+#                                width=200,
+#                                height=50,
+#                                fg_color="#FFF2F2",
+#                                border_width=3,
+#                                corner_radius=10)
 						
-txtReference.grid(row = 0, column = 1,pady=8)
+# txtReference.grid(row = 0, column = 1,pady=8)
 
 # labels
 lblMsg = customtkinter.CTkLabel(master=f1,
@@ -99,7 +98,7 @@ lblMsg = customtkinter.CTkLabel(master=f1,
                                width=200,
                                height=60, font = ('arial', 25))
 		
-lblMsg.grid(row = 1, column = 0,pady=8)
+lblMsg.grid(row = 0, column = 0,pady=50)
 
 txtMsg = customtkinter.CTkEntry(master=f1,
                                textvariable=Msg,
@@ -108,7 +107,7 @@ txtMsg = customtkinter.CTkEntry(master=f1,
                                fg_color="#FFF2F2",
                                border_width=3,
                                corner_radius=10)			
-txtMsg.grid(row = 1, column = 1,pady=8)
+txtMsg.grid(row = 0, column = 1,pady=8)
 
 lblkey = customtkinter.CTkLabel(master=f1,
                                text="გასაღები",
@@ -117,7 +116,7 @@ lblkey = customtkinter.CTkLabel(master=f1,
                                width=200,
                                height=60, font = ('arial', 25))
 			
-lblkey.grid(row = 2, column = 0,pady=8)
+lblkey.grid(row = 1, column = 0,pady=8)
 
 txtkey = customtkinter.CTkEntry(master=f1,
                                textvariable=key,
@@ -127,25 +126,24 @@ txtkey = customtkinter.CTkEntry(master=f1,
                                border_width=3,
                                corner_radius=10)
 				
-txtkey.grid(row = 2, column = 1,pady=8)
+txtkey.grid(row = 1, column = 1,pady=8)
 
 lblmode = customtkinter.CTkLabel(master=f1,
                                text="აირჩიეთ",
                                width=200,
-                                                              text_color="#FFFFE8",
-
+                                text_color="#FFFFE8",
                                height=60, font = ('arial', 25))
 								
-lblmode.grid(row = 3, column = 0,pady=5)
+lblmode.grid(row = 2, column = 0,pady=5)
 
 
 
 txtrdbtnmode1 =customtkinter.CTkRadioButton(master=f1, text="კოდირება", variable= mode,fg_color="#FF8B13", value=1)
-txtrdbtnmode1.grid(row = 3, column = 1,pady=5)
+txtrdbtnmode1.grid(row = 2, column = 1,pady=35)
 
 
 txtrdbtnmode2 =customtkinter.CTkRadioButton(master=f1, text="დეკოდირება", variable= mode,fg_color="#FF8B13", value=2)
-txtrdbtnmode2.grid(row = 3, column = 2,pady=5)
+txtrdbtnmode2.grid(row = 2, column = 2,pady=5)
                                           
 				
 
@@ -165,49 +163,49 @@ txtService.grid(row=0, column=0)
 
 
 # Vigenère cipher
-import base64
+def generateKey(string, key):
+	key = list(key)
+	if len(string) == len(key):
+		return(key)
+	else:
+		for i in range(len(string) -
+					len(key)):
+			key.append(key[i % len(key)])
+	return("" . join(key))
+
 
 # Function to encode
-def kodireba(key, clear):
-	enc = []
-	
-	for i in range(len(clear)):
-		key_c = key[i % len(key)]
-		enc_c = chr((ord(clear[i]) +
-					ord(key_c)) % 256)
-					
-		enc.append(enc_c)
-		
-	return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+def kodireba(string, key):
+	cipher_text = []
+	for i in range(len(string)):
+		x = (ord(string[i]) +
+			ord(key[i])) % 26
+		x += ord('A')
+		cipher_text.append(chr(x))
+	return("" . join(cipher_text))
 
 # Function to decode
 def dekodireba(key, enc):
-	dec = []
-	
-	enc = base64.urlsafe_b64decode(enc).decode()
+	orig_text = []
 	for i in range(len(enc)):
-		key_c = key[i % len(key)]
-		dec_c = chr((256 + ord(enc[i]) -
-						ord(key_c)) % 256)
-							
-		dec.append(dec_c)
-	return "".join(dec)
-
+		x = (ord(enc[i]) -
+			ord(key[i]) + 26) % 26
+		x += ord('A')
+		orig_text.append(chr(x))
+	return("" . join(orig_text))
 
 def Ref():
-    try:
         msg = Msg.get()
         k = key.get()
         m = mode.get()
-        if (not Msg.get() == "") and (not key.get() == "") and (not mode.get() == ""):
+        generateKeyWord = generateKey(msg,k)
+        if (not Msg.get() == "") and (not key.get() == "") and (not mode.get() == 0):
             if (m == 1):
-                Result.set(kodireba(k, msg))
+                Result.set(kodireba(generateKeyWord, msg))
             else:
-                Result.set(dekodireba(k, msg))
+                Result.set(dekodireba(generateKeyWord, msg))
         else:
             messagebox.showerror("showerror", "გთხოვთ შეავსოთ მოცემული ველები !")
-    except:
-        messagebox.showerror("showerror", "გთხოვთ აირჩიოთ კოდირება გსურთ , თუ დეკოდირება !")
 
 
 # Show message button
